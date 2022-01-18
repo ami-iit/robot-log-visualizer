62ed193f78d8b2a4921bc2aa1f9b6bb3a67378da
GiulioRomualdi <GiulioRomualdi@users.noreply.github.com>
2022-01-18 21:45:06 +0000

    ⚙️ Automatic update of the python UI classes

---
+++ ui/autogenerated/plot_tab.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'ui/misc/plot_tab.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.1
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_PlotTab(object):
+    def setupUi(self, PlotTab):
+        PlotTab.setObjectName("PlotTab")
+        PlotTab.resize(331, 149)
+        self.horizontalLayout = QtWidgets.QHBoxLayout(PlotTab)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.plotLayout = QtWidgets.QVBoxLayout()
+        self.plotLayout.setObjectName("plotLayout")
+        self.horizontalLayout.addLayout(self.plotLayout)
+
+        self.retranslateUi(PlotTab)
+        QtCore.QMetaObject.connectSlotsByName(PlotTab)
+
+    def retranslateUi(self, PlotTab):
+        _translate = QtCore.QCoreApplication.translate
+        PlotTab.setWindowTitle(_translate("PlotTab", "Form"))

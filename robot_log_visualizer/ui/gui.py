fa4282f9254f65e58a67bfbdc7549f33645cf087
Nick Tremaroli <nicholas.tremaroli@iit.it>
2024-05-17 15:11:14 +0200

    Fixed bug with text logging

--- robot_log_visualizer/ui/gui.py
+++ robot_log_visualizer/ui/gui.py
@@ -731,8 +731,8 @@ class RobotViewerMainWindow(QtWidgets.QMainWindow):
 
         # Do initial connection to populate the necessary data
         if not self.signal_provider.maintain_connection():
-            self.text_logger.add_entry("Could not connect to YARP server", time.time())
-            self.realtimeConnectionEnabled = False
+            self.logger.write_to_log("Could not connect to the real-time logger.")
+            self.realtime_connection_enabled = False
             return
         self.meshcat_provider._realtimeMeshUpdate = True
         # only display one root in the gui

41df273b24f161335c351a7a038a0440b650a346
Giulio Romualdi <giulio.romualdi@gmail.com>
2025-10-05 21:44:22 +0200

    🐛 Fix audio output and media content imports for compatibility with Qt versions

--- robot_log_visualizer/ui/video_item.py
+++ robot_log_visualizer/ui/video_item.py
@@ -3,7 +3,16 @@
 # Released under the terms of the BSD 3-Clause License
 
 from qtpy.QtCore import QUrl
-from qtpy.QtMultimedia import QAudioOutput, QMediaPlayer
+from qtpy.QtMultimedia import QMediaPlayer
+try:  # Qt 6
+    from qtpy.QtMultimedia import QAudioOutput  # type: ignore
+except ImportError:  # Qt 5 fallback
+    QAudioOutput = None  # type: ignore
+
+try:  # Qt 5 fallback
+    from qtpy.QtMultimedia import QMediaContent  # type: ignore
+except ImportError:  # Qt 6
+    QMediaContent = None  # type: ignore
 from qtpy.QtWidgets import QFrame
 
 from robot_log_visualizer.ui.ui_loader import load_ui
@@ -16,12 +25,25 @@ class VideoItem(QFrame):
         self.ui = load_ui("video_tab.ui", self)
 
         self.media_player = QMediaPlayer(self)
-        self.audio_output = QAudioOutput(self)
-        self.media_player.setAudioOutput(self.audio_output)
+
+        self.audio_output = None
+        if QAudioOutput is not None and hasattr(self.media_player, "setAudioOutput"):
+            self.audio_output = QAudioOutput(parent=self)
+            self.media_player.setAudioOutput(self.audio_output)
+
         self.media_player.setVideoOutput(self.ui.webcamView)
 
         self.media_loaded = False
 
         if os.path.isfile(video_filename):
-            self.media_player.setSource(QUrl.fromLocalFile(video_filename))
+            url = QUrl.fromLocalFile(video_filename)
+            if hasattr(self.media_player, "setSource"):
+                self.media_player.setSource(url)
+            elif hasattr(self.media_player, "setMedia"):
+                if QMediaContent is not None:
+                    self.media_player.setMedia(QMediaContent(url))
+                else:
+                    self.media_player.setMedia(url)
+            else:
+                raise AttributeError("QMediaPlayer backend missing media source setter")
             self.media_loaded = True
